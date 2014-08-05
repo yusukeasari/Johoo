17e5c3adf52f015ecd109d01f26fd740ac0fcc08
share <asari@pitmedia-marketings.com>
2014-08-05 16:52:51 +0900

    add solution mode

--- makeBgImage.php
+++ makeBgImage.php
@@ -6,6 +6,8 @@ RewriteCond %{REQUEST_FILENAME} !-f
 RewriteRule ^.*bg_(\w+)\.(jpg|gif|png)$ /makeBgImage.php?id=$1 [R]
 */
 
+//ソリューション時のbg.jpgを保存パスを記述する事
+$baseUrlForSolution = 'swfData/';
 
 if(!empty($_GET["id"])){
 	if(!file_exists("sp/swfData/mosaic/{$_GET["id"]}/bg_{$_GET["id"]}.jpg")){
@@ -65,4 +67,58 @@ if(!empty($_GET["id"])){
 		header("Content-type: image/jpeg");
 		echo file_get_contents("sp/swfData/mosaic/{$_GET["id"]}/bg_{$_GET["id"]}.jpg");
 	}
+}else{
+	if(!file_exists($baseUrlForSolution.'bg.jpg')){
+		$dirPath = "{$baseUrlForSolution}/web/1/";
+
+		$im = new Imagick();
+
+		$path = ".";
+		$iterator = new DirectoryIterator($dirPath);
+		$files = array();
+		$width = 0;
+		$height = 0;
+		$mwidth = 0;
+		$mheight = 0;
+
+		foreach ($iterator as $file) {
+			// ファイルがドットから始まっていない場合
+			// ディレクトリは除く
+			if (!$file->isDot() && $file->isDir()){
+				$mwidth=0;
+				foreach (new DirectoryIterator($file->getRealPath()) as $file2){
+					if(!$file2->isDot() && !$file2->isDir()){
+						$target = new Imagick($file2->getRealPath());
+
+						$mwidth += $target->getImageWidth();
+
+						array_push($files, array("image"=>$target,"width"=>$target->getImageWidth(),"height"=>$target->getImageHeight(),"name"=>$file2->getFilename()));
+					}
+				}
+				$mheight += $target->getImageHeight();
+				//
+			}else if(!$file->isDot() && !$file->isDir()){
+				array_push($files, $file->getRealPath().'/'.$file->getFilename);
+			}
+		}
+
+		$target = new Imagick();
+
+		//echo "{$mwidth}-{$mheight}";
+
+		$target->newImage($mwidth, $mheight, 'none');
+
+		$x=0;$y=0;
+		foreach ($files as $image) {
+			$x=substr($image["name"],3,1);
+			$y=substr($image["name"],5,1);
+			$target->compositeImage($image["image"], imagick::COMPOSITE_COPY, 256*$x, 256*$y);
+		}
+
+		$target->writeImage($baseUrlForSolution.'bg.jpg');
+		//$im->clear();
+		//$im->destroy();
+		//chmod('./uploads_orbi5000/text_img', 0666);
+		header("Content-type: image/jpeg");
+		echo file_get_contents($baseUrlForSolution.'bg.jpg');
 }
